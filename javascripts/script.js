87f1fbcc08cabd63759ca90afcaf76feb2aeb6f6
Tamara Lütscher <tamara.luetscher@gmail.com>
2014-05-23 10:51:04 +0200

    add dataType

--- javascripts/script.js
+++ javascripts/script.js
@@ -13,7 +13,7 @@ function success(pos) {
 
   	jQuery.ajax({
 	  	url: 'https://api.forecast.io/forecast/2440fc192add591a5ce89da2c8939529/37.8267,-122.423',
-	  	jsonp: 'callback',
+	  	dataType: 'jsonp',
 	  	success: function(data) {
 	  	console.log(data);
 	  	}
