141b1f4ce9fb47c4e962f1421a1bc783503d56ff
Tamara Lütscher <tamara.luetscher@gmail.com>
2014-05-23 10:24:12 +0200

    geändert

--- javascripts/script.js
+++ javascripts/script.js
@@ -7,10 +7,10 @@ var options = {
 function success(pos) {
   var crd = pos.coords;
 
-  console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
+  jquery (".longitude").text(crd.longitude);
+  jquery (".latitude").text(crd.latitude);
+  jquery (".accuracy").text(crd.accuracy + ' meters.');
+
 };
 
 function error(err) {
