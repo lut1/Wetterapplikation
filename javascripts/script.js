28d87ed14a2b622969eb22767fbb7048e438225b
Tamara Lütscher <tamara.luetscher@gmail.com>
2014-05-23 10:42:03 +0200

    add forecast service call

--- javascripts/script.js
+++ javascripts/script.js
@@ -11,6 +11,12 @@ function success(pos) {
   jQuery (".latitude").text(crd.latitude);
   jQuery (".accuracy").text(crd.accuracy + ' meters.');
 
+  	jQuery.ajax({
+	  	url: 'https://api.forecast.io/forecast/2440fc192add591a5ce89da2c8939529/37.8267,-122.423',
+	  	success: function(data) {
+	  	console.log(data);
+	  	}
+  	});
 };
 
 function error(err) {
